7fd24b4cd729db3fd65811c0b98c7d4a894db47c
kagika <r.m.kagika@gmail.com>
2026-06-10 20:45:20 +0300

    faah

--- lib/generate_log.py
+++ lib/generate_log.py
@@ -1,12 +1,22 @@
 from datetime import datetime
 import os
+import requests
 
 def generate_log(data):
     # TODO: Implement log generation logic
-
-    # STEP 1: Validate input
-    # Hint: Check if data is a list
-
+    
+    if not isinstance(data, list):
+        raise ValueError("Invalid input: log_data must be a list.")
+    date_str = datetime.now().strftime("%Y%m%d")
+    filename = f"log_{date_str}.txt"
+    
+    with open(filename, "w") as file:
+        for entry in data:
+            file.write(f"{entry}\n")
+    print(f"Log successfully written to {filename}")
+    
+    return filename
+    
     # STEP 2: Generate a filename with today's date (e.g., "log_20250408.txt")
     # Hint: Use datetime.now().strftime("%Y%m%d")
 
@@ -17,3 +27,20 @@ def generate_log(data):
     # STEP 4: Print a confirmation message with the filename
 
     pass
+
+def fetch_data():
+    response = requests.get("https://jsonplaceholder.typicode.com/posts/1")
+    if response.status_code == 200:
+        return response.json()
+    return {}
+
+if __name__ == "__main__":
+    post = fetch_data()
+    data = post.get("title", "No title found")
+    print("Fetched Post Title:",data )
+    displayed_data = [
+        "User updated",
+        "Data with title",
+        f"{data} has been created."
+    ]
+    generate_log(displayed_data)
